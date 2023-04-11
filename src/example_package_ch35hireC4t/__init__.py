9c37f41a07a32b9d8119459ba020f3fc37a18818
SimchaTeich <sim.teich@gmail.com>
2023-04-11 15:26:55 +0300

    update init

--- src/example_package_ch35hireC4t/__init__.py
+++ src/example_package_ch35hireC4t/__init__.py
@@ -1,1 +1,21 @@
+CHESHIRE_CAT = """Cheshire Cat by Joan Stark
+           .'\   /`.
+         .'.-.`-'.-.`.
+    ..._:   .-. .-.   :_...
+  .'    '-.(o ) (o ).-'    `.
+ :  _    _ _`~(_)~`_ _    _  :
+:  /:   ' .-=_   _=-. `   ;\  :
+:   :|-.._  '     `  _..-|:   :
+ :   `:| |`:-:-.-:-:'| |:'   :
+  `.   `.| | | | | | |.'   .'
+    `.   `-:_| | |_:-'   .'
+ jgs  `-._   ````    _.-'
+          ``-------''
+          
+https://www.asciiart.eu/books/alice-in-wonderland
+          """
+
+def print_cat():
+      print(CHESHIRE_CAT)
+
 
